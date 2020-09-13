a45cc8e6afafbae01d053e18c48bbe4aa04a3ae6
Derek-Huynh <derek@arch.localdomain>
2020-09-13 18:39:52 +0000

    i3 config changed and polybar added

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate any already running processes
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar config &
+
+echo "Polybar launched..."
+
